46852f8092c227970ebfff91bbd9ff9d52f1ca8a
em-phipho6 <emm.f.gh3618@gmail.com>
2024-10-22 15:44:32 -0600

    testing2
    trying to clone

--- Views/Form1.cs
+++ Views/Form1.cs
@@ -42,6 +42,7 @@ namespace aplicacionInventario4
       {
         tempFormCategorias = new FormsCategorias(this);
         tempFormCategorias.Show();
+        //hello
       }
       else
       {
--- obj/Debug/net8.0-windows/aplicacionInventario4.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/aplicacionInventario4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aplicacionInventario4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4161fba26080ccaaf9888aa93006af1a240209")]
 [assembly: System.Reflection.AssemblyProductAttribute("aplicacionInventario4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aplicacionInventario4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
