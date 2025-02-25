cb80a2d5614020476bd3e6247af334e1d45207a9
EmmaF535 <emm.f.gh3618@gmail.com>
2025-02-24 22:43:56 -0600

    changes
    connection between DB and project, trying to make it work

--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
+using DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.Extensions.Hosting;
+
 namespace aplicacionInventario4
 {
-  internal static class Program
-  {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-      // To customize application configuration such as set high DPI settings or default font,
-      // see https://aka.ms/applicationconfiguration.
-      ApplicationConfiguration.Initialize();
-      Application.Run(new Form1());
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+
+            var builder = Host.CreateDefaultBuilder(args);
+
+            builder.Build.AddDbContext<InventoryContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionStrings("InventoryConnection"));
+            });
+
+        }
     }
-  }
 }
