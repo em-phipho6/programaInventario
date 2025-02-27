978a45d681e687bb5ce78de6c26ba73c272e6a1c
EmmaF535 <emm.f.gh3618@gmail.com>
2025-02-26 23:38:56 -0600

    changes
    location file changed

---
+++ DB/Material.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class Material
+    {
+        public int IdMaterial { get; set; }
+        public string DescripcionMaterial { get; set; }
+        public double PrecioInterno { get; set; }
+        public double PrecioVenta { get; set; }
+        public int Cantidad { get; set; }
+        public int IdCategoria { get; set; }
+        
+        [ForeignKey("IdCategoria")]
+
+        public virtual Categoria Categoria { get; set; }
+
+
+    }
+
+}
